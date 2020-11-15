301b740ca2c70b1138ad437cfa4bdc41c1ea4ea3
MikeyBeez <mbonsign@gmail.com>
2020-11-15 00:52:25 -0500

    fixed bad filnames

--- startJulie-Julie.sh
+++ startJulie-Julie.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-/home/bard/miniconda3/envs/Julie-Julie/bin/python /home/bard/Code/Juliei-JulieJulie-Julie.py 2>/dev/null
+/home/bard/miniconda3/envs/Julie-Julie/bin/python /home/bard/Code/Julie-Julie/Julie-Julie.py 
+#2>/dev/null
 
