2a3b8f2670fd243f17abe24f0b534eb485d0a01e
MikeyBeez <mbonsign@gmail.com>
2020-11-15 00:03:03 -0500

    renamed Juliet files

---
+++ startJulieJulie.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/home/bard/miniconda3/envs/Julie-Julie/bin/python /home/bard/Code/Juliei-JulieJulie-Julie.py 2>/dev/null
+
--- startJuliet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/home/bard/miniconda3/envs/J2/bin/python /home/bard/Code/Juliet/Juliet.py 2>/dev/null
-
