beee8bfdd88e19db5782aa7fc3245d266a6a909a
MikeyBeez <mbonsign@gmail.com>
2020-11-17 14:40:54 -0500

    moved stuff into research-storage-attic

--- Melissa Stuff/message_checker.py
+++
@@ -1,6 +0,0 @@
-def message_checker(message, words):
-    words_of_message = message.split()
-    if set(words).issubset(set(words_of_message)):
-        return True
-    else:
-        return False
--- Melissa Stuff/profile.py
+++
@@ -1,8 +0,0 @@
-# Melissa
-from profile_loader import load_profile
-
-data = {}
-
-if len(data) == 0:
-    # global data
-    data = load_profile()
