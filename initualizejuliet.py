e9a43848d36456ef8d03a46b6c3addc28cb17e28
MikeyBeez <mbonsign@gmail.com>
2020-11-17 14:45:01 -0500

    changed model-en to model

--- initualizejuliet.py
+++ initualizejuliet.py
@@ -5,9 +5,9 @@ import os
 
 
 def CheckMyModel():
-    if not os.path.exists("model-en"):
+    if not os.path.exists("model"):
         print("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases")
-        print("and unpack as 'model-en' in the current folder.")
+        print("and unpack as 'model' in the current folder.")
         exit(1)
 ##############################################################
 # End Check Model.
